dd4c9fba0d93a7e2ca49d0142a6698ae0d52aebf
Billy <yliu179@sheffield.ac.uk>
2017-09-19 20:24:41 +0100

    update

---
+++ app/helpers/lien_items_helper.rb
@@ -0,0 +1,2 @@
+module LienItemsHelper
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,20 @@
-# Load the Rails application.
+ # Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+	config.action_mailer.delivery_method = :smtp
+
+	config.action_mailer.smtp_settings = {
+		address: 		"smtp.gmail.com",
+		port: 			587,
+		domain: 		"gmail.com",
+		authentication: "plain",
+		user_name: 		"billyliuyang@gmail.com",
+		password: 		"bengtengly1000",
+		enable_starttls_auto: true
+	}
+end
+
