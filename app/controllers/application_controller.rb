45329cdff54d3569049c7f705eafbc4dfbd26162
Billy <yliu179@sheffield.ac.uk>
2018-01-17 02:06:29 +0800

    gemfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authorize
+  # before_action :authorize
   before_action :set_i18n_locale_from_params
   protect_from_forgery with: :exception
 
